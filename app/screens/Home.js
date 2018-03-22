a7977f36b985727c088bee34c6686842a49d312e
Monk <sabintheworld@gmail.com>
2018-03-22 14:14:40 -0700

    Comments/Docs

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native'
 
 export default class HomeScreen extends React.Component {
     static navigationOptions = {
-        title: 'Welcome'
+        title: 'Home Page'
     }
     render() {
         return (
