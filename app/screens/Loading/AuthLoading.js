9e11260a466ee66b4ee240ab3c718ff82364a0d9
Monk <sabintheworld@gmail.com>
2018-03-22 22:29:44 -0700

    Commenting

--- app/screens/Loading/AuthLoading.js
+++ app/screens/Loading/AuthLoading.js
@@ -16,7 +16,7 @@ export default class AuthLoading extends React.Component {
     // Fetch the token from storage then navigate to our appropriate place
     _bootstrapAsync = async () => {
         const userToken = await AsyncStorage.getItem('userToken');
-
+        // Setting timeout to simulate loading
         setTimeout(() => {
             // This will switch to the App screen or Auth screen and this loading
             // screen will be unmounted and thrown away.
